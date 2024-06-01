e9b2b9b9caf8e97a019f5a8f5beaf55080c3f08b
tox-in <habimanat78@gmail.com>
2024-06-01 14:47:34 +0200

    final changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/Mongo_connect.js");
+const userRoute = require("./routes/users.js");
+const pinRoute = require("./routes/pins.js");
 
 dotenv.config();
 
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
 
 app.listen(8800, () => {
