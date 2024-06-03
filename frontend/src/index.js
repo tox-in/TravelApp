cff5a56bd4f9fc0e9a7e1fffdddd8d4dac9977b7
tox-in <habimanat78@gmail.com>
2024-06-03 16:52:53 +0200

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
 serviceWorker.unregister();
