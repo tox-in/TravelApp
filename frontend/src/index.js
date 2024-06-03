fd130d26ea06e767125314361c7ac947afb2acc6
tox-in <habimanat78@gmail.com>
2024-06-03 16:23:17 +0200

    new changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
